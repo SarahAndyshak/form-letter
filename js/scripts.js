6bfe086eb693f8d2a13a381d882be10cdd11883f
Sarah Andyshak <sarah.andyshak@gmail.com>
2023-01-11 11:49:58 -0800

    add missing const person1Input to scripts.js.

--- js/scripts.js
+++ js/scripts.js
@@ -1,8 +1,12 @@
 window.onload = function() {
   let form = document.querySelector("form");
   form.onsubmit = function(event) {
+    const person1Input = document.getElementById("person1Input").value;
+    
     document.querySelector("span#person1a").innerText = "person1";
 
+    document.querySelector("div#story").removeAttribute("class");
+    
     event.preventDefault();
   };
 };
